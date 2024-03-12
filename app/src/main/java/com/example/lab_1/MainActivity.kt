bc2eaef597b00f81ce3cd9471593998d6bfd7121
mrosasfe <01190771@pw.edu.pl>
2024-03-12 09:32:13 +0100

    .

---
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.lab_1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val goToNextActivityButton: Button = findViewById(R.id.btn1)
+        goToNextActivityButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lab_1/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.lab_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        var closeBtn = findViewById<Button>(android.R.id.button2);
+        closeBtn.setOnClickListener {
+            finish()
+
+        };
+    }
+}
